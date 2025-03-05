a6473edbd6bc52f5ba3dfc15551ef6b1de45dad7
BuffaZombie2005 <lengocn88@gmail.com>
2025-03-05 14:59:53 +0700

    Update draft.py

--- draft.py
+++ draft.py
@@ -2,7 +2,10 @@ S = 0
 
 N = int(input("Input N(N must bigger than 9) "))
 if N<10:
-    raise Exception("Sorry number must bigger than 9")
+    raise Exception("Sorry N must bigger than 9")
 for i in range (1,N+1):
-    S += i*10+i
+    if i<10:
+        S += i*10+i
+    else:
+        S += i*100+i
 print(S)
